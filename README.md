ce483eb2f6f4f7c80bfc739e7a2694eaa6abd7d6
Kagemand Andersen <trillykins@gmail.com>
2022-07-18 11:20:36 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# 12-to-24-chrome-converter
+
+Simple extension for Chromium-based browsers to convert AM/PM timestamps to 24-hour timestamps
+
+Toggle by simply disabling the extension (until I add a toggle button)
---
+++ content-script.js
@@ -0,0 +1,45 @@
+
+const text = document.querySelectorAll('h1,h2,h3,h4,h5,p,li,td,caption,span')
+const regex = new RegExp(/(0?[1-9]|1[0-2])(:([0-5]\d))?\s?((?:A|P)\.?M\.?)/gi)
+
+for (let i = 0; i < text.length; i++) {
+    if (regex.test(text[i].innerHTML)) {
+        // console.log(text[i].innerHTML)
+        const matches = text[i].innerHTML.match(regex)
+        for (const match of matches) {
+            // Converting rules taken from: https://www.freecodecamp.org/news/mathematics-converting-am-pm-to-24-hour-clock/
+            if (match.toLowerCase().endsWith('am')) {
+                // 12 AM: subtract 12 hours
+                if (match.startsWith("12")) {
+                    const replacement = match.toLowerCase().replace("12:", "00:").replace(" am", "");
+                    text[i].innerHTML = text[i].innerHTML.replace(match, replacement)
+                    console.log("Replaced (" + match + ") with (" + replacement + ")")
+                } else {
+                    // 01:00 AM - 12:59 PM: Same, just remove AM suffix
+                    const indexOfWhitespace = match.indexOf(" ")
+                    const replacement = match.substring(0, indexOfWhitespace)
+                    text[i].innerHTML = text[i].innerHTML.replace(match, replacement)
+                    console.log("Replaced (" + match + ") with (" + replacement + ")")
+                }
+            } else if (match.toLowerCase().endsWith('pm')) {
+                if (match.startsWith("12")) {
+                    // Same as above (just the 12 PM match)
+                    const indexOfWhitespace = match.indexOf(" ")
+                    const replacement = match.substring(0, indexOfWhitespace)
+                    text[i].innerHTML = text[i].innerHTML.replace(match, replacement)
+                    console.log("Replaced (" + match + ") with (" + replacement + ")")
+                } else {
+                    // 01:00 PM - 12:59 AM (add 12 hours)
+                    const indexOfColon = match.indexOf(":")
+                    let hours = parseInt(match.substring(0, indexOfColon))
+                    hours += 12
+                    const indexOfWhitespace = match.indexOf(" ")
+                    let replacement = match.substring(0, indexOfWhitespace)
+                    replacement = hours + replacement.substring(indexOfColon)
+                    text[i].innerHTML = text[i].innerHTML.replace(match, replacement)
+                    console.log("Replaced (" + match + ") with (" + replacement + ")")
+                }
+            }
+        }
+    }
+}
